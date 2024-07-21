b508f16a626a5fb88a8cfdb59da25917edb9eb66
NiharP31 <niharasare@gmail.com>
2024-07-21 16:19:31 -0400

    v1 code

---
+++ drowsiness_v1.py
@@ -0,0 +1,5 @@
+from andu_pandu import *
+
+data_path = r'C:\Users\nihar\Documents\github\Deep_learning\Driver_drowsiness\Driver Drowsiness Dataset (DDD)'
+
+drowsy_data_path = [os.path.join(data_path, '')]
